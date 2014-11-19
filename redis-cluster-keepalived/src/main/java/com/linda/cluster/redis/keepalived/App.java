460980159aebca80f54f257fc74e8c5cfd9fd8f2
linda <linsony0@163.com>
2014-11-19 10:07:03 +0800

    redis node keepalived

---
+++ redis-cluster-keepalived/src/main/java/com/linda/cluster/redis/keepalived/App.java
@@ -0,0 +1,13 @@
+package com.linda.cluster.redis.keepalived;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
